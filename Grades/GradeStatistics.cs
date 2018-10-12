e21bb0a5c5b150f071f7a46b74d1e8f5cb366993
Mark <iam@home.org>
2018-10-12 22:30:51 +0200

    AddLetterGrade

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -8,6 +8,30 @@ namespace Grades
 {
     public class GradeStatistics
     {
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (AverageGrade >= 90)
+                {
+                    result = "A";
+                } else if (AverageGrade >= 80)
+                {
+                    result = "B";
+                } else if (AverageGrade >= 70)
+                {
+                    result = "C";
+                } else if (AverageGrade >= 60)
+                {
+                    result = "D";
+                } else {
+                    result = "F";
+                }
+                return result;
+            }
+        }
+
         public GradeStatistics()
         {
             HighestGrade = 0;
